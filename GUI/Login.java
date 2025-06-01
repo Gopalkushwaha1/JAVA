0249c4d9a3b864197914e9bc0425b8ff3379849e
Gopalkushwaha1 <kushwahagopal2003@gmail.com>
2025-06-01 10:12:24 +0530

    Add GUI

---
+++ GUI/Login.java
@@ -0,0 +1,12 @@
+import javax.swing.* ; 
+
+class Login {
+	public static void main(String[] args) {
+		System.out.println("Main Block") ; 
+		JFrame j1 = new JFrame("Welcome to Login page " ) ; 
+		j1.setSize(500,500) ; 
+		j1.setVisible(true) ; 
+		j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ; 
+		
+	}
+}
